fdc399cb48bbac4c9564caa08714c16e36a5ac27
Bhaskar13 <bhaskar.singal@myntra.com>
2018-06-01 01:21:01 +0530

    changes

--- Backend/SpringApplication/src/test/java/com/application/dao/test/TrieTest.java
+++ Backend/SpringApplication/src/test/java/com/application/dao/test/TrieTest.java
@@ -8,13 +8,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.application.dao.Trie;
+import com.application.dao.TrieNode;
 import com.application.entity.Domain;
 
 
+@SpringBootTest(classes = { Trie.class})
 @RunWith(SpringRunner.class)
 public class TrieTest {
 
@@ -22,15 +24,25 @@ public class TrieTest {
 	private Trie trie;
 	
 	@Test
-    public void searchCorrect() {
+    public void suggestCorrect() {
 		List<Domain> result = new ArrayList<Domain>();
-		Domain domain = new Domain("zenefits.com",1);
+		Domain domain = new Domain("abcdefghijklmnopqrstuvwxyz.com",1000);
 		result.add(domain);
 		trie.insert(domain);
-        List<Domain> testName = trie.suggestSearch("zenefits");
-        System.out.println("#######:"+testName);
-        Assert.assertEquals(result, testName);
+        List<Domain> testName = trie.suggestSearch("abcdefghijklmnop");
+        Assert.assertArrayEquals(result.toArray(), testName.toArray());
     }
+	
+	@Test
+    public void searchCorrect() {
+		List<Domain> result = new ArrayList<Domain>();
+		Domain domain = new Domain("qwertyuiopasdfghjkl.com",1000);
+		result.add(domain);
+		trie.insert(domain);
+        List<Domain> testName = trie.search("qwertyuiopasdfg");
+        Assert.assertArrayEquals(result.toArray(), testName.toArray());
+    }
+	
 	
 
 }
